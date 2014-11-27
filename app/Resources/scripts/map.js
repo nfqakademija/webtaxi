50d24763f875c2c255c9ee794e198df73e1ee530
Domantas Varapnickas <domantasvarapnickas@gmail.com>
2014-11-27 19:00:03 +0200

    Add text fields for coordinates.

--- app/Resources/scripts/map.js
+++ app/Resources/scripts/map.js
@@ -87,9 +87,13 @@
             if (marker.title == 'start') {
                 document.getElementById('address').value = address;
                 start = marker.getPosition();
+                document.getElementById('start_lat').value =marker.getPosition().lat();
+                document.getElementById('start_lng').value =marker.getPosition().lng();
             } else {
                 document.getElementById('dest_address').value = address;
                 dest = marker.getPosition()
+                document.getElementById('dest_lat').value =marker.getPosition().lat();
+                document.getElementById('dest_lng').value =marker.getPosition().lng();
             }
 
             calcRoute(start, dest);
