7898389951cc4a5a14bf424417dfc019faa15cdb
Domantas Varapnickas <domantasvarapnickas@gmail.com>
2014-12-11 05:33:09 +0200

    Changed marker

--- app/Resources/scripts/map.js
+++ app/Resources/scripts/map.js
@@ -171,7 +171,7 @@
      */
     function createMarker(pos, t) {
         if(t==='start'){
-        var image = paramsObject.getAttribute('data-userIcon');
+        var image = paramsObject.getAttribute('data-userIcon1');
         } else {var image = paramsObject.getAttribute('data-finishIcon');}
 
             var marker = new google.maps.Marker({
